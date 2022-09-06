e707d5f78459782fe5c7e2036683e6aa1def854a
taoky <me@taoky.moe>
2022-09-06 12:52:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+A very simple GTK program to show a picture without window decoration in Linux (supports transparent PNG files). Written in gtk-rs.
+
+Hint: Press Alt + Space to activate mutter window menu in GNOME. You can set the window to be "Always on Top" there.
+
+## Usage
+
+```
+./showimg --file ./path/to/img
+```
+
+Picture will be shown with their natural size.
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use clap::Parser;
+use gtk::{gdk, prelude::*, CssProvider, Picture, StyleContext, STYLE_PROVIDER_PRIORITY_APPLICATION};
+use gtk::{Application, ApplicationWindow};
+use gtk::gio::File;
+
+const APP_ID: &str = "org.taoky.showimg";
+const CSS: &str = ".background {
+    background-color: rgba(0, 0, 0, 0);
+}";
+
+#[derive(Parser, Debug)]
+#[clap(about)]
+struct Args {
+    // The image file to open
+    #[clap(short, long, value_parser)]
+    file: String
+}
+
+fn main() {
+    // Parse args to get image
+    let args = Args::parse();
+
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(move |app: &Application| build_ui(app, &args.file));
+
+    // Run the application (bypass GTK command line handling)
+    app.run_with_args(&[""]);
+}
+
+fn build_ui(app: &Application, filename: &str) {
+    // CSS style
+    let css_provider = CssProvider::new();
+    css_provider.load_from_data(CSS.as_bytes());
+    StyleContext::add_provider_for_display(
+        &gdk::Display::default().expect("Error initializing gdk default display"),
+        &css_provider,
+        STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+
+    // Image
+    let texture = gdk::Texture::from_file(&File::for_path(filename)).expect("Cannot open image");
+    let image = Picture::builder()
+        .paintable(&texture)
+        .build();
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Show Img")
+        .child(&image)
+        .build();
+
+    window.set_decorated(false);
+
+    // Present window
+    window.present();
+}
